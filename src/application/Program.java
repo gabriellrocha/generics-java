c07e8254df8f54edbb95b4d2a07197a94689fa5a
Gabriel Rocha <gabriell_rsilva@outlook.com>
2023-02-20 11:58:23 -0300

    Create class PrintService

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,30 @@ package application;
 
 import java.util.Scanner;
 
+import services.PrintService;
+
 public class Program {
 	
 	public static void main(String[] agrs) {
 		
 		Scanner input = new Scanner(System.in);
 		
+		PrintService<Integer> ps = new PrintService<>();
 		
+		System.out.print("How many values? ");
+		int value = input.nextInt();
 		
+		for (int i=1; i <= value; i++) {
+			ps.addValue(input.nextInt());
+		}
 		
+		try {
+			System.out.println("First " + ps.first());
+		} catch(IllegalStateException e) {
+			System.out.println(e.getMessage());
+		}
 		
-		
+		ps.print();
 		
 		
 		input.close();
